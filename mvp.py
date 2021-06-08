5974289369363138853b949b81d1ab5a2d0536ca
Ji Yu <jyu@uchc.edu>
2021-06-08 12:58:22 -0400

    Add protocol 1 support

--- mvp.py
+++ mvp.py
@@ -1,12 +1,21 @@
 import serial
+import datatime
 
 class Mvp:
 	stx, etx, eot, enq, ack, nak = (b'\x02',b'\x03',b'\x04',b'\x05',b'\x06',b'\x15')
 
-	def __init__(self, port):
-		self.ser = serial.Serial(port, bytesize=7, parity=serial.PARITY_EVEN, stopbits=2, timeout=1.0)
+	def __init__(self, port, protocol = 'DIN'):
+		self.protocol = protocol
+		if protocol == 'DIN':
+			self.ser = serial.Serial(port, bytesize=7, parity=serial.PARITY_EVEN, stopbits=2, timeout=1.0)
+		else:
+			self.ser = serial.Serial(port, bytesize=7, parity=serial.PARITY_ODD, stopits=1, timeout=1.0)
+			#auto address
+			self.ser.write(b'1a')
+			self.ser.write(b'0x0d')
+		self.ser.resetInputBuffer()
 
-	def getBcc(self, s):
+	def _getBcc(self, s):
 		bcc = 0
 		for b in s:
 			bcc = bcc ^ b
@@ -14,38 +23,71 @@ class Mvp:
 		bcc = bcc ^ 255
 		return bytes([bcc])
 
-	def sendCmd(self, s):
-		self.ser.write(self.stx)
-		self.ser.write(s)
-		self.ser.write(self.etx)
-		self.ser.write(self.getBcc(s))
+	def _exception(self, s):
+		raise Exception(str(datetime.datetime.now()) + s)
 
-		return self.ser.read()
+	def sendCmd(self, s):
+		self.ser.resetInputBuffer()
+		if self.protocol == 'DIN':
+			self.ser.write(self.stx)
+			self.ser.write(s)
+			self.ser.write(self.etx)
+			self.ser.write(self._getBcc(s))
+			self.checkAck()
+		else:
+			self.ser.write(s)
+			self.ser.write(b'0x0d') #CR
+			self.checkAck()
 
 	def selectDev(self, n):
-		self.ser.write(b'0%i'%n)
-		self.ser.write(self.enq)
+		self.ser.resetInputBuffer()
+		if self.protocol == 'DIN':
+			self.ser.write(self.eot)
+			adr = b'%02i'%n
+			self.ser.write(adr)
+			self.ser.write(self.enq)
+			resp = self.ser.read(2)
+			if resp != adr:
+				self._exception('Addressing error: ' + adr.decode('ascii'))
+			self.checkAck()
+		else:
+			a = bytes([96 + n])
+			self.ser.write(a)
 
-		return self.ser.read()
+	def checkAck(self):
+		resp = self.ser.read()
+		if resp != self.ack:
+			self._exception("No ackowledgement from device.")
 
 	def readResp(self):
-		ans = self.ser.read()
-		if (ans != self.stx) :
-			return None
-		s = b''
-		ans = b''
-		while (ans != self.etx):
-			s += ans
+		if self.protocol == 'DIN':
 			ans = self.ser.read()
-		ans = serlf.ser.read()
-		if getBcc(s) != ans:
-			return None
-		else:
+			if (ans != self.stx) :
+				self._exception('Response does not start with STX')
+			resp = self.ser.read_until(self.etx)
+			bcc = self.ser.read()
+			if getBcc(resp) != ans:
+				self._exception('Incorrect BBC in response')
 			return s
+		else:
+			resp = self.ser.read_until(b'0x0d')
+			return resp
 
 	def setValvePosition(self, dev, pos):
-		self.ser.write(self.eot)
 		self.selectDev(dev)
-		self.sendCmd(b'Vn%i'%pos)
-		self.sendCmd(b'Ur')
-		self.sendCmd(self.eot)
+		if self.protocol == 'DIN':
+			self.sendCmd(b'Vn%i'%pos)
+			self.sendCmd(b'Ur')
+		else:
+			self.sendCmd(b'LP0%iR'%pos)
+
+	def getValvePosition(self, dev):
+		self.selectDev(dev)
+		if self.protocol = 'DIN':
+			self.sendCmd('Ap')
+			resp = self.readResp()
+			return int(resp[2:])
+		else:
+			self.sendCmd('LQP')
+			resp = self.readResp()
+			return(int(resp))
